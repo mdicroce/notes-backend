96ab42bc5130839442c98a6e037617fe8688a081
mbexel <md_dicroce@hotmail.com>
2021-04-14 17:26:23 -0300

    add heroku needed config

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /* const { response } = require('express') */
 let morgan = require('morgan');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 morgan.token('post', (res,req) => {
   if(res.method === "POST")
@@ -96,7 +98,7 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter((actual) => actual.id !== id);
   response.status(204).end();
 });
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
